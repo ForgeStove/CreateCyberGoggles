74c0a57e8a31bad0e736793769a1e7ec061664dc
ForgeStove <ForgeStove@users.noreply.github.com>
2025-03-07 00:50:47 +0800

    feat(core): 允许在开启光影的情况下强制启用飞轮优化

---
+++ src/main/java/com/ForgeStove/create_project/mixin/SimpleBackendMixin.java
@@ -0,0 +1,11 @@
+package com.ForgeStove.create_project.mixin;
+import dev.engine_room.flywheel.lib.backend.SimpleBackend;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+@Mixin(SimpleBackend.class) public abstract class SimpleBackendMixin {
+	@Inject(method = "isSupported", at = @At("HEAD"), remap = false, cancellable = true)
+	private void isSupported(CallbackInfoReturnable<Boolean> returnable) {
+		returnable.setReturnValue(true);
+	}
+}

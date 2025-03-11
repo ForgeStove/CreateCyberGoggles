0daa6b8b556f2b64c8d8efeb9ee34434f00c26dc
ForgeStove <ForgeStove@users.noreply.github.com>
2025-03-11 22:34:01 +0800

    feat: 添加CogWheelBlockMixin以强制返回有效的齿轮位置

---
+++ src/main/java/com/ForgeStove/create_project/mixin/CogWheelBlockMixin.java
@@ -0,0 +1,11 @@
+package com.ForgeStove.create_project.mixin;
+import com.simibubi.create.content.kinetics.simpleRelays.CogWheelBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+@Mixin(CogWheelBlock.class) public abstract class CogWheelBlockMixin {
+	@Inject(method = "isValidCogwheelPosition", at = @At("HEAD"), cancellable = true)
+	private static void isValidCogwheelPosition(CallbackInfoReturnable<Boolean> returnable) {
+		returnable.setReturnValue(true);
+	}
+}

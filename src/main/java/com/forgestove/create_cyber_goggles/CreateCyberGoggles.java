58c6de82035e6dad697f31cc30137a2b8bdbe18e
ForgeStove <ForgeStove@users.noreply.github.com>
2025-03-30 19:22:59 +0800

    fix: 修正键输入和事件处理方法名

--- src/main/java/com/forgestove/create_cyber_goggles/CreateCyberGoggles.java
+++ src/main/java/com/forgestove/create_cyber_goggles/CreateCyberGoggles.java
@@ -20,18 +20,20 @@ import org.jetbrains.annotations.NotNull;
 		modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
 	}
 	@EventBusSubscriber(value = Dist.CLIENT, bus = Bus.GAME) public static class ClientGameEvents {
-		@SubscribeEvent public static void onKeyInput(Key event) {
+		@SubscribeEvent public static void key(Key event) {
 			KeyInput.openFilterScreen(event);
 			KeyInput.openConfigScreen(event);
 		}
-		@SubscribeEvent public static void onMouseScroll(MouseScrollingEvent event) {MoseScroll.onMouseScroll(event);}
-		@SubscribeEvent public static void onCloseScreen(Closing event) {CloseScreen.onCloseScreen(event);}
+		@SubscribeEvent public static void mouseScrollingEvent(MouseScrollingEvent event) {
+			MoseScroll.onMouseScroll(event);
+		}
+		@SubscribeEvent public static void closing(Closing event) {CloseScreen.onCloseScreen(event);}
 	}
 	@EventBusSubscriber(value = Dist.CLIENT, bus = Bus.MOD) public static class ClientModEvents {
-		@SubscribeEvent public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+		@SubscribeEvent public static void registerKeyMappingsEvent(RegisterKeyMappingsEvent event) {
 			KeyBind.register(event);
 		}
-		@SubscribeEvent public static void registerGuiOverlays(RegisterGuiLayersEvent event) {
+		@SubscribeEvent public static void registerGuiLayersEvent(RegisterGuiLayersEvent event) {
 			OverlayRenderer.register(event);
 		}
 	}

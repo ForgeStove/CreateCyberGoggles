ceca7aa3ef8ec2294592544875acc94b61fe1f23
ForgeStove <ForgeStove@users.noreply.github.com>
2025-05-13 22:40:40 +0800

    Refactor CCGConfig to use SafeRun for configuration registration and improve null handling

--- src/main/java/com/forgestove/create_cyber_goggles/content/config/CCGConfig.java
+++ src/main/java/com/forgestove/create_cyber_goggles/content/config/CCGConfig.java
@@ -1,17 +1,17 @@
 package com.forgestove.create_cyber_goggles.content.config;
+import com.forgestove.create_cyber_goggles.content.util.SafeRun;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory;
 import net.minecraftforge.fml.ModLoadingContext;
 public class CCGConfig {
-	private static CCGConfigData config;
-	public static CCGConfigData get() {
-		if (config == null) config = AutoConfig.register(CCGConfigData.class, Toml4jConfigSerializer::new).getConfig();
-		return config;
-	}
+	static CCGConfigData config;
 	public static void register() {
-		config = AutoConfig.register(CCGConfigData.class, Toml4jConfigSerializer::new).getConfig();
+		SafeRun.run(() -> config = AutoConfig.register(CCGConfigData.class, Toml4jConfigSerializer::new).getConfig());
 		var factory = new ConfigScreenFactory((mc, screen) -> AutoConfig.getConfigScreen(CCGConfigData.class, screen).get());
 		ModLoadingContext.get().registerExtensionPoint(ConfigScreenFactory.class, () -> factory);
+	}
+	public static CCGConfigData get() {
+		return config != null ? config : (config = AutoConfig.register(CCGConfigData.class, Toml4jConfigSerializer::new).getConfig());
 	}
 }

034256fe081fb61c04b20ada11279b32d0b2fb8d
ForgeStove <ForgeStove@users.noreply.github.com>
2025-05-22 01:34:20 +0800

    chore: update Java toolchain version to use dynamic property

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 base.archivesName.set(e("mod_id"))
 group = e("mod_group_id")
 version = "${e("minecraft_version")}-${e("mod_version")}+${e("upper_loader")}"
-java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(e("javaVersion")))
 idea.module {
 	isDownloadSources = true
 	isDownloadJavadoc = true

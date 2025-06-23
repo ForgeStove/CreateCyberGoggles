d8a88e5fab7ab952afd8872743ea1b0476406df9
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-23 23:19:40 +0800

    将 Registrate Maven URL 更新为 ithundxr 镜像

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,7 +51,7 @@ repositories {
 	mavenLocal()
 	mavenCentral()
 	maven("https://maven.createmod.net") // Create, Ponder, Flywheel
-	maven("https://maven.tterrag.com") // Registrate
+	maven("https://maven.ithundxr.dev/mirror") // Registrate
 	maven("https://maven.shedaniel.me") // Cloth Config API
 	maven("https://maven.blamejared.com") // JEI
 	maven("https://api.modrinth.com/maven") // Modrinth

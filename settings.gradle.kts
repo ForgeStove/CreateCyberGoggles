9b0fcb435fa35283f8b567c2a9f9f382fd2b43b3
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-27 00:52:28 +0800

    更新build.gradle.kts以使用raw GitHub Maven并修正依赖项声明

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,6 @@ pluginManagement {
 	repositories {
 		mavenLocal()
 		gradlePluginPortal()
-		maven("https://raw.githubusercontent.com/ForgeStove/Maven/main/releases")
 	}
 }
 plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "+" }

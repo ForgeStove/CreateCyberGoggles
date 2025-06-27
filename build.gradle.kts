3270eaa477ef5aaba303c287e3f63d7751ec94f8
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-27 16:19:02 +0800

    更新build.gradle.kts以使用property方法替代extra方法，优化依赖项声明

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,13 @@
 @file:Suppress("SpellCheckingInspection", "UnstableApiUsage")
 
 plugins {
-	idea
 	id("fabric-loom") version "+"
 	id("me.modmuss50.mod-publish-plugin") version "+"
 }
-base.archivesName.set(e("mod_id"))
-group = e("mod_group_id")
-version = "${e("minecraft_version")}-${e("mod_version")}+${e("upper_loader")}"
+base.archivesName.set(p("mod_id"))
+group = p("mod_group_id")
+version = "${p("minecraft_version")}-${p("mod_version")}+${p("upper_loader")}"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-idea.module {
-	isDownloadSources = true
-	isDownloadJavadoc = true
-}
 tasks.jar { from("LICENSE") }
 tasks.processResources {
 	val replace = properties.mapValues { it.value.toString() }
@@ -24,8 +19,8 @@ tasks.processResources {
 	into("build/resources/main")
 	duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
-configurations.configureEach { resolutionStrategy.force("net.fabricmc:fabric-loader:${e("fabric_loader_version")}") }
-loom.accessWidenerPath.set(file("src/main/resources/${e("mod_id")}.accesswidener"))
+configurations.configureEach { resolutionStrategy.force("net.fabricmc:fabric-loader:${p("fabric_loader_version")}") }
+loom.accessWidenerPath.set(file("src/main/resources/${p("mod_id")}.accesswidener"))
 repositories {
 	mavenLocal()
 	mavenCentral()
@@ -41,38 +36,38 @@ repositories {
 	maven("https://maven.blamejared.com") // JEI
 }
 dependencies {
-	minecraft("com.mojang:minecraft:${e("minecraft_version")}")
+	minecraft("com.mojang:minecraft:${p("minecraft_version")}")
 	mappings(loom.layered {
 		officialMojangMappings { nameSyntheticMembers = false }
-		parchment("org.parchmentmc.data:parchment-${e("minecraft_version")}:${e("parchment_version")}@zip")
+		parchment("org.parchmentmc.data:parchment-${p("minecraft_version")}:${p("parchment_version")}@zip")
 	})
-	modImplementation("net.fabricmc:fabric-loader:${e("fabric_loader_version")}")
-	modImplementation("net.fabricmc.fabric-api:fabric-api:${e("fabric_api_version")}")
-	modImplementation("com.simibubi.create:create-${e("loader")}-${e("minecraft_version")}:${e("create_version")}")
-	modImplementation("me.shedaniel.cloth:cloth-config-${e("loader")}:${e("cloth_config_version")}")
-	modImplementation("com.terraformersmc:modmenu:${e("modmenu_version")}")
-	modImplementation("mezz.jei:jei-${e("minecraft_version")}-${e("loader")}:${e("jei_version")}")
+	modImplementation("net.fabricmc:fabric-loader:${p("fabric_loader_version")}")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${p("fabric_api_version")}")
+	modImplementation("com.simibubi.create:create-${p("loader")}-${p("minecraft_version")}:${p("create_version")}")
+	modImplementation("me.shedaniel.cloth:cloth-config-${p("loader")}:${p("cloth_config_version")}")
+	modImplementation("com.terraformersmc:modmenu:${p("modmenu_version")}")
+	modImplementation("mezz.jei:jei-${p("minecraft_version")}-${p("loader")}:${p("jei_version")}")
 }
 publishMods {
 	file.set(tasks.remapJar.get().archiveFile)
 	changelog.set(file("CHANGELOG.md").readText())
 	type.set(STABLE)
 	version.set(project.version.toString())
-	displayName.set("[${e("upper_loader")}] ${e("mod_name")} ${e("mod_version")}+${e("minecraft_version")}")
-	modLoaders.addAll(e("upper_loader"), e("other_loader"))
+	displayName.set("[${p("upper_loader")}] ${p("mod_name")} ${p("mod_version")}+${p("minecraft_version")}")
+	modLoaders.addAll(p("upper_loader"), p("other_loader"))
 	modrinth {
 		accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
 		projectId.set("TlQAWQCY")
-		minecraftVersions.add(e("minecraft_version"))
+		minecraftVersions.add(p("minecraft_version"))
 		requires("create-fabric", "cloth-config")
 		optional("modmenu")
 	}
 	curseforge {
 		accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
 		projectId.set("1233804")
-		minecraftVersions.add(e("minecraft_version"))
+		minecraftVersions.add(p("minecraft_version"))
 		requires("create-fabric", "cloth-config")
 		optional("modmenu")
 	}
 }
-fun e(key: String) = extra[key].toString()
+fun p(key: String) = property(key).toString()

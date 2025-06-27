ab4b19272997d6b04ea15e2e2b5fd78a1c7fbaec
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-27 16:08:33 +0800

    更新build.gradle.kts以使用property方法替代extra方法，优化依赖项声明

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 	id("me.modmuss50.mod-publish-plugin") version "+"
 	id("io.github.forgestove.modaccessor")
 }
-base.archivesName.set(e("mod_id"))
-group = e("mod_group_id")
-version = "${e("minecraft_version")}-${e("mod_version")}+${e("upper_loader")}"
+base.archivesName.set(p("mod_id"))
+group = p("mod_group_id")
+version = "${p("minecraft_version")}-${p("mod_version")}+${p("upper_loader")}"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 tasks.jar { from("LICENSE") }
 tasks.processResources {
@@ -21,10 +21,10 @@ tasks.processResources {
 	duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
 neoForge {
-	version = e("loader_version")
+	version = p("loader_version")
 	parchment {
-		mappingsVersion.set(e("parchment_version"))
-		minecraftVersion.set(e("minecraft_version"))
+		mappingsVersion.set(p("parchment_version"))
+		minecraftVersion.set(p("minecraft_version"))
 	}
 	runs {
 		create("client").client()
@@ -33,7 +33,7 @@ neoForge {
 			systemProperty("terminal.jline", "true")
 		}
 	}
-	mods.create(e("mod_id")).sourceSet(sourceSets["main"])
+	mods.create(p("mod_id")).sourceSet(sourceSets["main"])
 }
 repositories {
 	mavenLocal()
@@ -45,35 +45,35 @@ repositories {
 	maven("https://api.modrinth.com/maven") // Modrinth
 }
 dependencies {
-	accessCompileOnly("com.simibubi.create:create-${e("minecraft_version")}:${e("create_version")}:slim")
-	runtimeOnly("com.simibubi.create:create-${e("minecraft_version")}:${e("create_version")}:slim") { isTransitive = false }
-	implementation("net.createmod.ponder:Ponder-${e("upper_loader")}-${e("minecraft_version")}:${e("ponder_version")}") {
+	accessCompileOnly("com.simibubi.create:create-${p("minecraft_version")}:${p("create_version")}:slim")
+	runtimeOnly("com.simibubi.create:create-${p("minecraft_version")}:${p("create_version")}:slim") { isTransitive = false }
+	implementation("net.createmod.ponder:Ponder-${p("upper_loader")}-${p("minecraft_version")}:${p("ponder_version")}") {
 		isTransitive = false
 	}
-	implementation("dev.engine-room.flywheel:flywheel-${e("loader")}-${e("minecraft_version")}:${e("flywheel_version")}")
-	implementation("com.tterrag.registrate:Registrate:${e("registrate_version")}")
-	implementation("me.shedaniel.cloth:cloth-config-${e("loader")}:${e("cloth_config_version")}")
-	implementation("mezz.jei:jei-${e("minecraft_version")}-${e("loader")}:${e("jei_version")}")
-	implementation("maven.modrinth:jade:${e("jade_version")}")
+	implementation("dev.engine-room.flywheel:flywheel-${p("loader")}-${p("minecraft_version")}:${p("flywheel_version")}")
+	implementation("com.tterrag.registrate:Registrate:${p("registrate_version")}")
+	implementation("me.shedaniel.cloth:cloth-config-${p("loader")}:${p("cloth_config_version")}")
+	implementation("mezz.jei:jei-${p("minecraft_version")}-${p("loader")}:${p("jei_version")}")
+	implementation("maven.modrinth:jade:${p("jade_version")}")
 }
 publishMods {
 	file.set(tasks.jar.get().archiveFile)
 	changelog.set(file("CHANGELOG.md").readText())
 	type.set(STABLE)
 	version.set(project.version.toString())
-	displayName.set("[${e("upper_loader")}] ${e("mod_name")} ${e("mod_version")}+${e("minecraft_version")}")
-	modLoaders.addAll(e("upper_loader"))
+	displayName.set("[${p("upper_loader")}] ${p("mod_name")} ${p("mod_version")}+${p("minecraft_version")}")
+	modLoaders.addAll(p("upper_loader"))
 	modrinth {
 		accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
 		projectId.set("TlQAWQCY")
-		minecraftVersions.add(e("minecraft_version"))
+		minecraftVersions.add(p("minecraft_version"))
 		requires("create", "cloth-config")
 	}
 	curseforge {
 		accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))
 		projectId.set("1233804")
-		minecraftVersions.add(e("minecraft_version"))
+		minecraftVersions.add(p("minecraft_version"))
 		requires("create", "cloth-config")
 	}
 }
-fun e(key: String) = extra[key].toString()
+fun p(key: String) = property(key).toString()

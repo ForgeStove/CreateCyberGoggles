a0789ddd356acc98fea57e626ebda8e341fbb1ae
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-28 22:03:51 +0800

    修复因移除super限定符引起的StackOverFlowError #19

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/goggles/DeployerBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/goggles/DeployerBlockEntityMixin.java
@@ -44,6 +44,6 @@ public abstract class DeployerBlockEntityMixin extends KineticBlockEntity {
 	)
 	private void addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking, CallbackInfoReturnable<Boolean> returnable) {
 		if (!CCG.CONFIG.goggles.enhancedInfo) return;
-		returnable.setReturnValue(addToGoggleTooltip(tooltip, isPlayerSneaking));
+		returnable.setReturnValue(super.addToGoggleTooltip(tooltip, isPlayerSneaking));
 	}
 }

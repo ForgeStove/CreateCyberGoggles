090ef1a4871aaf606f69670b58e69f95eade975c
ForgeStove <ForgeStove@users.noreply.github.com>
2025-06-30 10:48:38 +0800

    要求依赖更新为create-0.5.1j

--- build.gradle.kts
+++ build.gradle.kts
@@ -72,7 +72,11 @@ publishMods {
 		accessToken.set(providers.environmentVariable("MODRINTH_TOKEN"))
 		projectId.set("TlQAWQCY")
 		minecraftVersions.add(p("minecraft_version"))
-		requires("6R069CcK", "cloth-config")
+		requires {
+			id.set("LNytGWDc")
+			version.set("6R069CcK")
+		}
+		requires("cloth-config")
 	}
 	curseforge {
 		accessToken.set(providers.environmentVariable("CURSEFORGE_TOKEN"))

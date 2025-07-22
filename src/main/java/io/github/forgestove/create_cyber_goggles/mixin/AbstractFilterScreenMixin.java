7293350a35a9ad2e45d1b0566e5f1c3feb3d780c
ForgeStove <ForgeStove@users.noreply.github.com>
2025-07-22 18:55:34 +0800

    使用WrapWithCondition简化WrapOperation

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/AbstractFilterScreenMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/AbstractFilterScreenMixin.java
@@ -1,5 +1,5 @@
 package io.github.forgestove.create_cyber_goggles.mixin;
-import com.llamalad7.mixinextras.injector.wrapoperation.*;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.simibubi.create.content.logistics.filter.*;
 import com.simibubi.create.foundation.gui.menu.AbstractSimiContainerScreen;
 import net.minecraft.network.chat.Component;
@@ -11,14 +11,14 @@ public abstract class AbstractFilterScreenMixin<F extends AbstractFilterMenu> ex
 	public AbstractFilterScreenMixin(F container, Inventory inv, Component title) {
 		super(container, inv, title);
 	}
-	@WrapOperation(
+	@WrapWithCondition(
 		method = "containerTick", at = @At(
 		value = "INVOKE",
 		target = "Lio/github/fabricators_of_create/porting_lib/util/PlayerEntityHelper;closeScreen"
 			+ "(Lnet/minecraft/world/entity/player/Player;)V"
 	)
 	)
-	public void containerTick(Player instance, Operation<Void> original) {
-		if (menu.containerId != -1) original.call(instance);
+	public boolean containerTick(Player instance) {
+		return menu.containerId != -1;
 	}
 }

4c47b58e3bf8aa64f4fe6f108212f6ac14c9b1c9
ForgeStove <ForgeStove@users.noreply.github.com>
2025-07-30 12:48:02 +0800

    更新processResources任务以确保输出始终更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,10 @@ group = p("mod_group_id")
 version = "${p("minecraft_version")}-${p("mod_version")}+${p("upper_loader")}"
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(21))
 tasks.jar { from("LICENSE") }
-tasks.processResources { filesMatching("META-INF/neoforge.mods.toml") { expand(properties) } }
+tasks.processResources {
+    outputs.upToDateWhen { false }
+    filesMatching("META-INF/neoforge.mods.toml") { expand(properties) }
+}
 neoForge {
 	version = p("loader_version")
 	parchment {

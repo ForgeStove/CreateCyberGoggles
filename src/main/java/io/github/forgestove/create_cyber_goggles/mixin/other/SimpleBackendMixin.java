01e91762134f4a1539d8a1bf9d95a5d95a688616
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-03 18:51:16 +0800

    移除不必要的工具方法

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/other/SimpleBackendMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/other/SimpleBackendMixin.java
@@ -1,7 +1,6 @@
 package io.github.forgestove.create_cyber_goggles.mixin.other;
 import dev.engine_room.flywheel.lib.backend.SimpleBackend;
 import io.github.forgestove.create_cyber_goggles.CCG;
-import io.github.forgestove.create_cyber_goggles.util.SafeRun;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -9,6 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class SimpleBackendMixin {
 	@Inject(method = "isSupported", at = @At("HEAD"), cancellable = true)
 	private void isSupported(CallbackInfoReturnable<Boolean> returnable) {
-		SafeRun.run(() -> {if (CCG.CONFIG.other.forcedBackend) returnable.setReturnValue(true);});
+		if (CCG.CONFIG.other.forcedBackend) returnable.setReturnValue(true);
 	}
 }

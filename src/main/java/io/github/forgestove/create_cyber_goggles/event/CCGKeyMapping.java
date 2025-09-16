edff9971ad166bc242acb43e45f383ea99abb51b
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-16 17:58:26 +0800

    refactor(io.github.forgestove.create_cyber_goggles.event): 优化键位映射类 CCGKeyMapping
    - 移除了未使用的导入项和注解- 简化了 CCGKeyMapping 枚举类的结构
    - 重写了 isDown 方法，使用 KeyMapping 内置的 isDown 方法替代手动 GLFW调用

--- src/main/java/io/github/forgestove/create_cyber_goggles/event/CCGKeyMapping.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/event/CCGKeyMapping.java
@@ -1,11 +1,10 @@
 package io.github.forgestove.create_cyber_goggles.event;
 import com.mojang.blaze3d.platform.InputConstants.Type;
 import io.github.forgestove.create_cyber_goggles.CCG;
-import net.minecraft.client.*;
+import net.minecraft.client.KeyMapping;
 import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.common.util.Lazy;
-import org.jetbrains.annotations.*;
 import org.lwjgl.glfw.GLFW;
 public enum CCGKeyMapping {
 	openConfig(GLFW.GLFW_KEY_UNKNOWN),
@@ -14,7 +13,6 @@ public enum CCGKeyMapping {
 	toggleDiving(GLFW.GLFW_KEY_UNKNOWN),
 	showStress(GLFW.GLFW_KEY_TAB);
 	private final Lazy<KeyMapping> keyMapping;
-	@Contract(pure = true)
 	CCGKeyMapping(int key) {
 		keyMapping = Lazy.of(() -> new KeyMapping(
 			"key." + CCG.ID + "." + name(),
@@ -27,12 +25,10 @@ public enum CCGKeyMapping {
 	public static void register(RegisterKeyMappingsEvent event) {
 		for (var key : values()) event.register(key.get());
 	}
-	public @NotNull KeyMapping get() {
+	public KeyMapping get() {
 		return keyMapping.get();
 	}
 	public boolean isDown() {
-		var value = get().getKey().getValue();
-		if (value == GLFW.GLFW_KEY_UNKNOWN) return false;
-		return GLFW.glfwGetKey(Minecraft.getInstance().getWindow().getWindow(), value) == GLFW.GLFW_PRESS;
+		return keyMapping.get().isDown();
 	}
 }

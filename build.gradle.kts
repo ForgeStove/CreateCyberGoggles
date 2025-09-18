7cc36accef6dd69a8a3ec34ba1186e84aea763d7
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-18 13:01:47 +0800

    更新构建脚本以清理生成的元数据

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,11 @@ var generateMetadata = tasks.register<ProcessResources>("generateMetadata") {
 	from("src/main/templates")
 	into("build/generated/sources/modMetadata")
 }
+var cleanMetadata = tasks.register<Delete>("cleanMetadata") {
+	delete("build/generated/sources/modMetadata")
+}
 sourceSets.main.get().resources.srcDir(generateMetadata)
-neoForge.ideSyncTasks.add(generateMetadata)
+neoForge.ideSyncTasks.addAll(cleanMetadata, generateMetadata)
 neoForge {
 	version = p("loader_version")
 	parchment {

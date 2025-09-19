c1c2c6225cb8f258532fbedf759e9afb953bded0
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-19 11:33:31 +0800

    修复按键bug

--- src/main/java/io/github/forgestove/create_cyber_goggles/event/CCGKey.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/event/CCGKey.java
@@ -1,15 +1,17 @@
 package io.github.forgestove.create_cyber_goggles.event;
+import com.mojang.blaze3d.platform.InputConstants;
 import io.github.forgestove.create_cyber_goggles.CCG;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.KeyMapping;
+import net.minecraft.client.*;
 import org.lwjgl.glfw.GLFW;
 public enum CCGKey {
 	openConfig(GLFW.GLFW_KEY_UNKNOWN),
-	openStock(GLFW.GLFW_KEY_UNKNOWN),
 	previewFilter(GLFW.GLFW_KEY_UNKNOWN),
 	toggleDiving(GLFW.GLFW_KEY_UNKNOWN),
 	showStress(GLFW.GLFW_KEY_TAB);
 	public final KeyMapping keyMapping;
+	private boolean wasDown = false;
+	private long pressStartTime;
 	CCGKey(int key) {
 		keyMapping = new KeyMapping(CCG.ID + ".key." + name(), key, "key.categories." + CCG.ID);
 	}
@@ -17,6 +19,16 @@ public enum CCGKey {
 		for (var key : values()) KeyBindingHelper.registerKeyBinding(key.keyMapping);
 	}
 	public boolean isKeyDown() {
-		return keyMapping.isDown();
+		var isDown = InputConstants.isKeyDown(Minecraft.getInstance().getWindow().getWindow(), keyMapping.key.getValue());
+		var currentTime = System.currentTimeMillis();
+		if (isDown && !wasDown) {
+			wasDown = true;
+			pressStartTime = currentTime;
+			return true;
+		} else if (isDown) return currentTime - pressStartTime >= 500;
+		else {
+			wasDown = false;
+			return false;
+		}
 	}
 }

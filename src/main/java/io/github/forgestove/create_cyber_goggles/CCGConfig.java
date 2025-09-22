c391447c3b064f141ccf213c4aed4e334ad2cd07
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-22 23:56:46 +0800

    新增无限编辑框长度选项并实现相关Mixin

--- src/main/java/io/github/forgestove/create_cyber_goggles/CCGConfig.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/CCGConfig.java
@@ -51,6 +51,7 @@ public class CCGConfig implements ConfigData {
 		@Tooltip public boolean removeCooldown = true;
 	}
 	public static class Misc {
+		@Tooltip public boolean infEditBoxLength = true;
 		@Tooltip public boolean removeCardboardOverlay = true;
 		@Tooltip public boolean removeNetheriteFirstPerson = false;
 		@Tooltip public boolean removeDivingFunction = false;
---
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/EditBoxMixin.java
@@ -0,0 +1,16 @@
+package io.github.forgestove.create_cyber_goggles.mixin.misc;
+import io.github.forgestove.create_cyber_goggles.CCG;
+import net.minecraft.client.gui.components.EditBox;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+@Mixin(EditBox.class)
+public abstract class EditBoxMixin {
+	@Shadow private int maxLength;
+	@Inject(method = "setMaxLength", at = @At("HEAD"), cancellable = true)
+	private void setMaxLength(int maxLength, CallbackInfo callbackInfo) {
+		if (!CCG.CONFIG.misc.infEditBoxLength) return;
+		callbackInfo.cancel();
+		this.maxLength = Integer.MAX_VALUE;
+	}
+}

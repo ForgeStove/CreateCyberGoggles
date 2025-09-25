aa5bb561e2bceed2a21ff5eca2714b6d6194b37d
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-25 23:44:32 +0800

    修复蓝图不能大写的问题

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/FilesHelperMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/FilesHelperMixin.java
@@ -1,7 +1,6 @@
 package io.github.forgestove.create_cyber_goggles.mixin.misc;
 import com.simibubi.create.foundation.utility.FilesHelper;
 import io.github.forgestove.create_cyber_goggles.CCG;
-import net.createmod.catnip.lang.Lang;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -9,7 +8,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public abstract class FilesHelperMixin {
 	@Inject(method = "slug", at = @At("HEAD"), cancellable = true)
 	private static void slug(String name, CallbackInfoReturnable<String> returnable) {
-		if (!CCG.CONFIG.misc.fixSchematicName) return;
-		returnable.setReturnValue(Lang.asId(name).replaceAll("[\\\\/:*?\"<>|]+", "_"));
+		if (CCG.CONFIG.misc.fixSchematicName) returnable.setReturnValue(name.replaceAll("[\\\\/:*?\"<>|]+", "_"));
 	}
 }

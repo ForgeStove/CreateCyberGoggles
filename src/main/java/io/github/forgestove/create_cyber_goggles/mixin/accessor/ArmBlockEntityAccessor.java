2a753e522191f5c84e5a29a3146ed8e7a657d2c3
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-26 11:17:34 +0800

    新增 DeployerBlockEntityAccessor 并优化机械臂和部署器 heldItem 获取逻辑，完善进度条显示

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/accessor/ArmBlockEntityAccessor.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/accessor/ArmBlockEntityAccessor.java
@@ -1,5 +1,6 @@
 package io.github.forgestove.create_cyber_goggles.mixin.accessor;
 import com.simibubi.create.content.kinetics.mechanicalArm.*;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -10,4 +11,6 @@ public interface ArmBlockEntityAccessor {
 	List<ArmInteractionPoint> getInputs();
 	@Accessor
 	List<ArmInteractionPoint> getOutputs();
+	@Accessor
+	ItemStack getHeldItem();
 }

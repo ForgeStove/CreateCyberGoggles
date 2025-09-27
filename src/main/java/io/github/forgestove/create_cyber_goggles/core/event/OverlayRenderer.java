29a0e4d71b77b829e209f46ae09c13f7dedc7c25
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-27 09:46:38 +0800

    优化护目镜物品提示渲染逻辑，调整坐标计算方式及悬停偏移

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/event/OverlayRenderer.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/event/OverlayRenderer.java
@@ -1,5 +1,4 @@
 package io.github.forgestove.create_cyber_goggles.core.event;
-import com.simibubi.create.content.equipment.goggles.GoggleOverlayRenderer;
 import com.simibubi.create.foundation.blockEntity.behaviour.ValueBox;
 import com.simibubi.create.infrastructure.config.AllConfigs;
 import io.github.forgestove.create_cyber_goggles.CCG;
@@ -83,16 +82,13 @@ public class OverlayRenderer {
 		if (itemStack == null || itemStack.isEmpty()) return;
 		var mc = Minecraft.getInstance();
 		var font = mc.font;
-		var tooltipFlag = new Default(mc.options.advancedItemTooltips, true);
-		var tooltipLines = itemStack.getTooltipLines(TooltipContext.of(mc.level), mc.player, tooltipFlag);
-		var height = tooltipLines.size() * font.lineHeight;
+		var flag = new Default(mc.options.advancedItemTooltips, true);
+		var tooltip = itemStack.getTooltipLines(TooltipContext.of(mc.level), mc.player, flag);
 		var cfg = AllConfigs.client();
-		var offsetX = fade < 1 ? (int) (
-			Math.pow(1 - fade, 3) * Math.signum(cfg.overlayOffsetX.get() + .5f) * 8
-		) : 0;
-		var x = guiGraphics.guiWidth() / 2 + cfg.overlayOffsetX.get() + offsetX;
+		var x = guiGraphics.guiWidth() / 2 + cfg.overlayOffsetX.get();
 		var y = guiGraphics.guiHeight() / 2 + cfg.overlayOffsetY.get();
-		if (GoggleOverlayRenderer.hoverTicks != 0) y -= height + 20;
+		x += fade < 1 ? (int) (Math.pow(1 - fade, 3) * Math.signum(cfg.overlayOffsetX.get() + .5f) * 8) : 0;
+		if (hoverTicks != 0) y -= (tooltip.size() + 1) * 10;
 		guiGraphics.renderItem(itemStack, x - 10, y - 10);
 		guiGraphics.renderItemDecorations(font, itemStack, x - 10, y - 10);
 		guiGraphics.renderTooltip(font, itemStack, x, y);

650008e014809fbc5f3f84379141e84515241a2b
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-27 22:19:16 +0800

    新增BacktankBlockEntityMixin与BacktankItemMixin以支持空气容量信息显示

--- src/main/java/io/github/forgestove/create_cyber_goggles/CCGLang.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/CCGLang.java
@@ -1,18 +1,25 @@
 package io.github.forgestove.create_cyber_goggles;
 import net.createmod.catnip.lang.*;
-import org.jetbrains.annotations.*;
+import net.minecraft.ChatFormatting;
+import org.jetbrains.annotations.Contract;
 public class CCGLang extends Lang {
 	@Contract(value = " -> new", pure = true)
-	public static @NotNull LangBuilder builder() {
+	public static LangBuilder builder() {
 		return new LangBuilder(CCG.ID);
 	}
-	public static @NotNull LangBuilder translate(String langKey, Object... args) {
+	public static LangBuilder translate(String langKey, Object... args) {
 		return builder().translate(langKey, args);
 	}
-	public static @NotNull LangBuilder number(double number) {
-		return builder().text(LangNumberFormat.format(number));
-	}
-	public static @NotNull LangBuilder text(String text) {
+	public static LangBuilder text(String text) {
 		return builder().text(text);
+	}
+	public static LangBuilder text(ChatFormatting format, String literalText) {
+		return builder().text(format, literalText);
+	}
+	public static LangBuilder text(int color, String literalText) {
+		return builder().text(color, literalText);
+	}
+	public static LangBuilder number(double n) {
+		return builder().text(LangNumberFormat.format(n));
 	}
 }

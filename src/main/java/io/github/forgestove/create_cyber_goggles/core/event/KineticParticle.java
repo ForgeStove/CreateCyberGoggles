1986536911a55a684b6d59a70300fe64c57ce82e
ForgeStove <ForgeStove@users.noreply.github.com>
2025-09-27 16:55:16 +0800

    优化KineticParticle粒子生成逻辑，调整事件类型及粒子位置分布

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/event/KineticParticle.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/event/KineticParticle.java
@@ -5,32 +5,37 @@ import io.github.forgestove.create_cyber_goggles.CCG;
 import io.github.forgestove.create_cyber_goggles.core.util.CCGUtil;
 import net.createmod.catnip.math.VecHelper;
 import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.event.RenderLevelStageEvent;
-import net.minecraftforge.client.event.RenderLevelStageEvent.Stage;
+import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.event.TickEvent.ClientTickEvent;
 public class KineticParticle {
-	public static void tick(RenderLevelStageEvent event) {
+	public static void tick(ClientTickEvent ignoredEvent) {
 		if (!CCG.CONFIG.goggles.enableKineticEffect) return;
-		if (event.getStage() != Stage.AFTER_PARTICLES) return;
+		var mc = Minecraft.getInstance();
+		if (mc.level == null || mc.screen != null) return;
 		var kbe = CCGUtil.getKBE();
 		if (kbe == null) return;
-		var mc = Minecraft.getInstance();
-		if (mc.level == null) return;
 		var speed = kbe.getSpeed();
 		if (speed == 0) return;
 		var state = kbe.getBlockState();
 		if (!(state.getBlock() instanceof KineticBlock kb)) return;
-		var rotationAxis = kb.getRotationAxis(state);
-		if (rotationAxis == null) return;
+		var axis = kb.getRotationAxis(state);
+		if (axis == null) return;
 		var center = VecHelper.getCenterOf(kbe.getBlockPos());
 		var speedLevel = SpeedLevel.of(speed);
 		var particleData = new RotationIndicatorParticleData(
 			speedLevel.getColor(),
 			Math.max(15, speedLevel.getParticleSpeed()) * Math.signum(speed),
-			kb.getParticleInitialRadius(),
-			kb.getParticleTargetRadius(),
-			20,
-			rotationAxis.name().charAt(0)
+			kb.getParticleInitialRadius() / 2,
+			kb.getParticleTargetRadius() / 2,
+			10,
+			axis.name().charAt(0)
 		);
-		mc.level.addParticle(particleData, center.x, center.y, center.z, 0, 0, 0);
+		var offset = 0.5;
+		var axisVec = new Vec3(axis == Axis.X ? offset : 0, axis == Axis.Y ? offset : 0, axis == Axis.Z ? offset : 0);
+		for (var i = -1; i <= 1; i += 2) {
+			var pos = center.add(axisVec.scale(i));
+			for (var j = 0; j < 3; j++) mc.level.addParticle(particleData, pos.x, pos.y, pos.z, 0, 0, 0);
+		}
 	}
 }

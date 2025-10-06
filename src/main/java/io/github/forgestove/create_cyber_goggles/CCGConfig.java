bec2afdf2dd32fe6275498ba190a2d1a4722d0f6
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-06 17:49:37 +0800

    移除enchancedWrench属性的重启要求

--- src/main/java/io/github/forgestove/create_cyber_goggles/CCGConfig.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/CCGConfig.java
@@ -49,7 +49,7 @@ public class CCGConfig implements ConfigData {
 		@Tooltip public boolean cardBoardedYourself = false;
 	}
 	public static class Wrench {
-		@Tooltip @RequiresRestart public boolean enchancedWrench = false;
+		@Tooltip public boolean enchancedWrench = false;
 		@Tooltip public boolean betterEncasedPipe = true;
 		@Tooltip public boolean alwaysAllowRotating = true;
 		@Tooltip public boolean leftClickFastDismantle = true;

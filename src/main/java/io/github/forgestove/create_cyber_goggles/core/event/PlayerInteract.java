860636fe9467fbe695575ab1c654d6222d3de015
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-08 00:23:00 +0800

    优化 PlayerInteract

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/event/PlayerInteract.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/event/PlayerInteract.java
@@ -9,20 +9,21 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.*;
 import net.neoforged.neoforge.client.event.ClientTickEvent.Post;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent.*;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.stream.Stream;
 
 import static io.github.forgestove.create_cyber_goggles.core.util.CCGUtil.*;
 import static net.neoforged.neoforge.event.entity.player.PlayerInteractEvent.LeftClickBlock.Action.*;
 public class PlayerInteract {
-	private static long lastDismantleTime;
-	private static long dismantleDelay = 10;
+	private static long lastDismantleTime, dismantleDelay = 10;
 	public static void tick(Post ignoredEvent) {
 		if (dismantleDelay < 10) dismantleDelay++;
 	}
-	public static void leftClick(@NotNull LeftClickBlock event) {
+	public static void leftClick(LeftClickBlock event) {
 		if (isServer()) return;
+		wrench(event);
+	}
+	private static void wrench(LeftClickBlock event) {
 		if (!CCG.CONFIG.wrench.leftClickFastDismantle) return;
 		if (dismantleDelay > 0) dismantleDelay--;
 		var canDismantle = System.currentTimeMillis() - lastDismantleTime > dismantleDelay * 20;
@@ -46,16 +47,19 @@ public class PlayerInteract {
 	}
 	public static void rightClick(RightClickBlock event) {
 		if (isServer()) return;
+		enacesdPipe(event);
+	}
+	private static void enacesdPipe(RightClickBlock event) {
 		if (!CCG.CONFIG.wrench.betterEncasedPipe) return;
+		var pos = event.getPos();
+		var state = event.getLevel().getBlockState(pos);
+		if (!(state.getBlock() instanceof EncasedPipeBlock)) return;
 		if (event.getHand() != InteractionHand.MAIN_HAND) return;
 		if (mc.player == null) return;
 		var anyMatch = Stream.of(mc.player.getMainHandItem(), mc.player.getOffhandItem())
 			.map(ItemStack::getItem)
 			.anyMatch(item -> item instanceof BlockItem || item instanceof WrenchItem || item instanceof DebugStickItem);
 		if (anyMatch) return;
-		var pos = event.getPos();
-		var state = event.getLevel().getBlockState(pos);
-		if (!(state.getBlock() instanceof EncasedPipeBlock)) return;
 		var clickedFace = event.getHitVec().getDirection();
 		if (mc.player.isShiftKeyDown()) clickedFace = clickedFace.getOpposite();
 		var property = EncasedPipeBlock.FACING_TO_PROPERTY_MAP.get(clickedFace);

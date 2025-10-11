65fe1df7cf36115ca0df81c0fc88098b0e82a2d1
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-12 00:32:51 +0800

    为getBlockEntity和getBlock方法添加@Nullable注解，以提高类型安全性

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/util/CCGUtil.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/util/CCGUtil.java
@@ -95,7 +95,7 @@ public class CCGUtil {
 	 * @param <T>   方块实体类型
 	 * @return 如果类型匹配则返回对应实例，否则返回{@code null}
 	 */
-	public static <T extends BlockEntity> T getBlockEntity(Class<T> clazz) {
+	public static <T extends BlockEntity> @Nullable T getBlockEntity(Class<T> clazz) {
 		return getAs(clazz, getBlockEntity());
 	}
 	/**
@@ -116,7 +116,7 @@ public class CCGUtil {
 	 * @param <T>   方块类型
 	 * @return 如果类型匹配则返回对应实例，否则返回{@code null}
 	 */
-	public static <T extends Block> T getBlock(Class<T> clazz) {
+	public static <T extends Block> @Nullable T getBlock(Class<T> clazz) {
 		return getAs(clazz, getBlock());
 	}
 	/**

ee61d7be8df1d9c299df563b6801006970c4f8f6
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-12 13:17:19 +0800

    重构KineticParticle中的粒子渲染逻辑，允许回退默认粒子渲染

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/event/KineticParticle.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/event/KineticParticle.java
@@ -6,14 +6,15 @@ import io.github.forgestove.create_cyber_goggles.mixin.accessor.RotationPropagat
 import net.createmod.catnip.math.VecHelper;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Direction.Axis;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.event.TickEvent.ClientTickEvent;
+import org.jetbrains.annotations.NotNull;
 
 import static io.github.forgestove.create_cyber_goggles.core.util.CCGUtil.*;
 public class KineticParticle {
 	public static void tick(ClientTickEvent ignoredEvent) {
-		if (!CCG.CONFIG.goggles.enableKineticEffect) return;
-		if (mc.level == null || isInGUI()) return;
+		if (!CCG.CONFIG.goggles.enableKineticEffect || mc.level == null || isInGUI()) return;
 		var kbe = getBlockEntity(KineticBlockEntity.class);
 		if (kbe == null) return;
 		var kbeSpeed = kbe.getSpeed();
@@ -22,20 +23,44 @@ public class KineticParticle {
 		if (!(state.getBlock() instanceof KineticBlock kb)) return;
 		var center = VecHelper.getCenterOf(kbe.getBlockPos());
 		var speedLevel = SpeedLevel.of(kbeSpeed);
-		var color = speedLevel.getColor();
 		var particleSpeed = Math.max(15, speedLevel.getParticleSpeed()) * Math.signum(kbeSpeed);
-		var radiusInitial = Math.max(0.3f, kb.getParticleInitialRadius() / 2);
-		var radiusTarget = Math.max(0.3f, kb.getParticleTargetRadius() / 2);
+		if (renderShaftParticles(kbe, kb, state, center, speedLevel.getColor(), particleSpeed)) return;
+		renderDefaultParticles(kb, state, center, speedLevel.getColor(), particleSpeed);
+	}
+	private static boolean renderShaftParticles(
+		KineticBlockEntity kbe,
+		KineticBlock kb,
+		BlockState state,
+		Vec3 center,
+		int color,
+		float particleSpeed
+	) {
+		var hasRendered = false;
 		for (var direction : Direction.values()) {
 			if (!kb.hasShaftTowards(mc.level, kbe.getBlockPos(), state, direction)) continue;
 			var axis = direction.getAxis();
 			var directionSpeed = particleSpeed * RotationPropagatorAccessor.getAxisModifier(kbe, direction);
-			var charAxis = axis.name().charAt(0);
-			var particleData = new RotationIndicatorParticleData(color, directionSpeed, radiusInitial, radiusTarget, 10, charAxis);
 			var offset = 0.5 * direction.getAxisDirection().getStep();
 			var axisVec = new Vec3(axis == Axis.X ? offset : 0, axis == Axis.Y ? offset : 0, axis == Axis.Z ? offset : 0);
 			var pos = center.add(axisVec);
-			for (var i = 0; i < 3; i++) mc.level.addParticle(particleData, pos.x, pos.y, pos.z, 0, 0, 0);
+			var initial = Math.max(0.3f, kb.getParticleInitialRadius() / 2);
+			var target = Math.max(0.3f, kb.getParticleTargetRadius() / 2);
+			var particleData = new RotationIndicatorParticleData(color, directionSpeed, initial, target, 10, axis.name().charAt(0));
+			spawnParticles(particleData, pos);
+			hasRendered = true;
 		}
+		//noinspection ConstantValue
+		return hasRendered;
+	}
+	private static void renderDefaultParticles(@NotNull KineticBlock kb, BlockState state, Vec3 center, int color, float particleSpeed) {
+		var initial = kb.getParticleInitialRadius();
+		var target = kb.getParticleTargetRadius();
+		var axisChar = kb.getRotationAxis(state).name().charAt(0);
+		var particleData = new RotationIndicatorParticleData(color, particleSpeed, initial, target, 10, axisChar);
+		spawnParticles(particleData, center);
+	}
+	private static void spawnParticles(RotationIndicatorParticleData particleData, Vec3 pos) {
+		if (mc.level == null) return;
+		for (var i = 0; i < 3; i++) mc.level.addParticle(particleData, pos.x, pos.y, pos.z, 0, 0, 0);
 	}
 }

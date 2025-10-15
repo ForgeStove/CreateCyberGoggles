8b1ca81c856aaf06ddbdeb895b6479b722ed364e
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-15 14:26:22 +0800

    简化代码结构

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/TableClothBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/TableClothBlockEntityMixin.java
@@ -1,25 +1,20 @@
 package io.github.forgestove.create_cyber_goggles.mixin.provider;
-import com.simibubi.create.content.logistics.redstoneRequester.AutoRequestData;
 import com.simibubi.create.content.logistics.tableCloth.TableClothBlockEntity;
 import io.github.forgestove.create_cyber_goggles.core.util.*;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.*;
-
-import java.util.List;
-@Mixin(value = TableClothBlockEntity.class, remap = false)
+@Mixin(TableClothBlockEntity.class)
 public abstract class TableClothBlockEntityMixin implements IItemRenderable, IItemIndex {
 	@Unique public int ccg$index;
-	@Shadow public AutoRequestData requestData;
-	@Shadow public List<ItemStack> manuallyAddedItems;
-	@Shadow
-	public abstract boolean isShop();
 	@Override
 	public ItemStack ccg$getItemStack() {
-		List<ItemStack> items;
-		if (!isShop()) items = manuallyAddedItems;
-		else items = requestData.encodedRequest.stacks().stream().map(bigItemStack -> bigItemStack.stack).toList();
+		var tcbe = (TableClothBlockEntity) (Object) this;
+		var isShop = tcbe.isShop();
+		var items = isShop
+			? tcbe.requestData.encodedRequest.stacks().stream().map(bigItemStack -> bigItemStack.stack).toList()
+			: tcbe.manuallyAddedItems;
 		if (items.isEmpty()) return null;
-		if (!isShop()) return items.get(items.size() - 1);
+		if (!isShop) return items.get(items.size() - 1);
 		if (ccg$index >= items.size()) ccg$index = 0;
 		return items.get(ccg$index);
 	}

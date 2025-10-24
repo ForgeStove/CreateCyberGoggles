7fd890e2418ebff62d06aaa6cbe960843e83db4a
ForgeStove <ForgeStove@users.noreply.github.com>
2025-10-24 12:50:15 +0800

    移除BlazeEnchanterBlockEntityMixin对BlazeExperienceBlockEntity的继承，简化类结构

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/BlazeEnchanterBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/BlazeEnchanterBlockEntityMixin.java
@@ -1,20 +1,14 @@
 package io.github.forgestove.create_cyber_goggles.mixin.provider;
 import io.github.forgestove.create_cyber_goggles.core.util.IItemRenderable;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntityType;
-import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.*;
-import plus.dragons.createenchantmentindustry.common.fluids.experience.BlazeExperienceBlockEntity;
 import plus.dragons.createenchantmentindustry.common.processing.enchanter.*;
+@Pseudo
 @Mixin(BlazeEnchanterBlockEntity.class)
-public abstract class BlazeEnchanterBlockEntityMixin extends BlazeExperienceBlockEntity implements IItemRenderable {
+public abstract class BlazeEnchanterBlockEntityMixin implements IItemRenderable {
 	@Unique public ItemStack ccg$cachedResult = ItemStack.EMPTY;
 	@Shadow protected EnchanterBehaviour enchanter;
 	@Shadow protected ItemStack heldItem;
-	public BlazeEnchanterBlockEntityMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
-		super(type, pos, state);
-	}
 	@Shadow
 	public abstract boolean isActive();
 	@Override

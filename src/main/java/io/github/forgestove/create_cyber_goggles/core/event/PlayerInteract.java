62c39fbbda9a4a728f5027943e686d795745c784
ForgeStove <ForgeStove@users.noreply.github.com>
2025-12-05 12:41:35 +0800

    更新PlayerInteract.java，添加对AllTags的支持以增强对可用工具的检测

--- src/main/java/io/github/forgestove/create_cyber_goggles/core/event/PlayerInteract.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/core/event/PlayerInteract.java
@@ -1,4 +1,5 @@
 package io.github.forgestove.create_cyber_goggles.core.event;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.content.contraptions.chassis.*;
 import com.simibubi.create.content.contraptions.wrench.RadialWrenchMenuSubmitPacket;
 import com.simibubi.create.content.equipment.wrench.*;
@@ -58,7 +59,10 @@ public class PlayerInteract {
 			? InteractionHand.MAIN_HAND
 			: player.getOffhandItem().getItem() instanceof WrenchItem ? InteractionHand.OFF_HAND : null;
 		if (handWithWrench == null) return;
-		if (!(getBlock() instanceof IWrenchable)) return;
+		var pos = event.getPos();
+		var state = event.getLevel().getBlockState(pos);
+		var block = state.getBlock();
+		if (!(block instanceof IWrenchable || AllTags.AllBlockTags.WRENCH_PICKUP.matches(state))) return;
 		var result = getBlockHitResult();
 		if (result == null) return;
 		sendAction(Action.PRESS_SHIFT_KEY);

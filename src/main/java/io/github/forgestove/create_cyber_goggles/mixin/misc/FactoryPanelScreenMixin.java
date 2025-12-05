5de75dd2b651ce24ece84960185cca06559fa21b
ForgeStove <ForgeStove@users.noreply.github.com>
2025-12-05 13:15:38 +0800

    更新请求限制选项，优化快速滚动功能

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/FactoryPanelScreenMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/FactoryPanelScreenMixin.java
@@ -1,12 +1,19 @@
 package io.github.forgestove.create_cyber_goggles.mixin.misc;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.simibubi.create.content.logistics.BigItemStack;
 import com.simibubi.create.content.logistics.factoryBoard.FactoryPanelScreen;
 import io.github.forgestove.create_cyber_goggles.CCG;
+import net.minecraft.world.item.Item;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 @Mixin(FactoryPanelScreen.class)
 public abstract class FactoryPanelScreenMixin {
 	@ModifyConstant(method = "mouseScrolled", constant = @Constant(intValue = 64))
-	private int modifyMaxScrollAmount(int original) {
+	public int modifyMaxScrollAmount(int original) {
 		return CCG.CONFIG.misc.removeRequestLimit ? Integer.MAX_VALUE : original;
+	}
+	@ModifyConstant(method = "mouseScrolled", constant = @Constant(intValue = 10))
+	public int modifyPerScrollAmount(int original, @Local(name = "itemStack") BigItemStack itemStack) {
+		return CCG.CONFIG.misc.removeRequestLimit ? Item.DEFAULT_MAX_STACK_SIZE - (itemStack.count == 1 ? 1 : 0) : original;
 	}
 }

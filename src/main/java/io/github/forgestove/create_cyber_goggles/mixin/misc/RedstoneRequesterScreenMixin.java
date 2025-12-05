14f45ada2ef5b4bb06c8dadce6591888a8d23e48
ForgeStove <ForgeStove@users.noreply.github.com>
2025-12-05 13:15:38 +0800

    更新请求限制选项，优化快速滚动功能

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/RedstoneRequesterScreenMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/RedstoneRequesterScreenMixin.java
@@ -1,12 +1,21 @@
 package io.github.forgestove.create_cyber_goggles.mixin.misc;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.zurrtum.create.client.content.logistics.redstoneRequester.RedstoneRequesterScreen;
 import io.github.forgestove.create_cyber_goggles.CCG;
-import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.world.item.Item;
+import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
+
+import java.util.List;
 @Mixin(RedstoneRequesterScreen.class)
 public abstract class RedstoneRequesterScreenMixin {
+	@Shadow private List<Integer> amounts;
 	@ModifyConstant(method = "mouseScrolled", constant = @Constant(intValue = 256))
-	private int modifyMaxScrollAmount(int original) {
+	public int modifyMaxScrollAmount(int original) {
 		return CCG.CONFIG.misc.removeRequestLimit ? Integer.MAX_VALUE : original;
+	}
+	@ModifyConstant(method = "mouseScrolled", constant = @Constant(intValue = 10))
+	public int modifyPerScrollAmount(int original, @Local(name = "i") int i) {
+		return CCG.CONFIG.misc.removeRequestLimit ? Item.DEFAULT_MAX_STACK_SIZE - (amounts.get(i) == 1 ? 1 : 0) : original;
 	}
 }

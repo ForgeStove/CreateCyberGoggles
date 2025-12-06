5917d9d0e04f261eaa056ea179cbac710cceb295
ForgeStove <ForgeStove@users.noreply.github.com>
2025-12-06 18:40:23 +0800

    修复缓存导致的显示错误

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/EjectorBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/EjectorBlockEntityMixin.java
@@ -13,7 +13,6 @@ import org.spongepowered.asm.mixin.*;
 import static io.github.forgestove.create_cyber_goggles.core.util.CCGUtil.*;
 @Mixin(EjectorBlockEntity.class)
 public abstract class EjectorBlockEntityMixin implements ItemRenderable, OutlineRenderable, Self<EjectorBlockEntity> {
-	@Unique public EntityLauncher ccg$launcher;
 	@Shadow DepotBehaviour depotBehaviour;
 	@Shadow
 	protected abstract Direction getFacing();
@@ -34,15 +33,15 @@ public abstract class EjectorBlockEntityMixin implements ItemRenderable, Outline
 		var xDiff = targetPos.getX() - blockPos.getX();
 		var yDiff = targetPos.getY() - blockPos.getY();
 		var zDiff = targetPos.getZ() - blockPos.getZ();
-		if (ccg$launcher == null) ccg$launcher = new EntityLauncher(Math.abs(xDiff + zDiff), yDiff);
-		var totalFlyingTicks = ccg$launcher.getTotalFlyingTicks() + 3;
+		var launcher = new EntityLauncher(Math.abs(xDiff + zDiff), yDiff);
+		var totalFlyingTicks = launcher.getTotalFlyingTicks() + 3;
 		var segments = (int) totalFlyingTicks / 3 + 1;
 		var tickOffset = totalFlyingTicks / segments;
 		var data = new DustParticleOptions(new Color(color).asVectorF(), 1);
 		if (mc.level == null) return;
 		for (var i = 0; i < segments; i++) {
 			var ticks = (AnimationTickHolder.getRenderTime() / 3) % tickOffset + i * tickOffset;
-			var vec = ccg$launcher.getGlobalPos(ticks, getFacing().getOpposite(), blockPos);
+			var vec = launcher.getGlobalPos(ticks, getFacing().getOpposite(), blockPos);
 			mc.level.addParticle(data, vec.x, vec.y, vec.z, 0, 0, 0);
 		}
 	}

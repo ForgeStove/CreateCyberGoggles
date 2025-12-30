104946fb8c50775b92b4129f074e50c17c927606
ForgeStove <ForgeStove@users.noreply.github.com>
2025-12-30 15:53:09 +0800

    为脉冲红石元件添加脉冲进度显示

---
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/BrassDiodeBlockEntityMixin.java
@@ -0,0 +1,19 @@
+package io.github.forgestove.create_cyber_goggles.mixin.provider;
+import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
+import com.simibubi.create.content.redstone.diodes.BrassDiodeBlockEntity;
+import com.simibubi.create.foundation.blockEntity.behaviour.scrollValue.ScrollValueBehaviour;
+import io.github.forgestove.create_cyber_goggles.core.util.GoggleTooltipUtil;
+import net.minecraft.network.chat.Component;
+import org.spongepowered.asm.mixin.*;
+
+import java.util.List;
+@Mixin(value = BrassDiodeBlockEntity.class, remap = false)
+public abstract class BrassDiodeBlockEntityMixin implements IHaveGoggleInformation {
+	@Shadow protected int state;
+	@Shadow ScrollValueBehaviour maxState;
+	@Override
+	public boolean addToGoggleTooltip(List<Component> tooltip, boolean isPlayerSneaking) {
+		GoggleTooltipUtil.pulse(tooltip, state, maxState.getValue());
+		return true;
+	}
+}

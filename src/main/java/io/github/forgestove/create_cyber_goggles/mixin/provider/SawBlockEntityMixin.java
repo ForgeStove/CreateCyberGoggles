4f2b892bcad2c22a1a0651fe8733d027c7c3db4e
ForgeStove <ForgeStove@users.noreply.github.com>
2026-03-12 13:53:31 +0800

    优化动力锯信息提示

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/SawBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/provider/SawBlockEntityMixin.java
@@ -7,6 +7,12 @@ import org.spongepowered.asm.mixin.Mixin;
 public abstract class SawBlockEntityMixin implements ItemRenderable, Self<SawBlockEntity> {
 	@Override
 	public ItemStack ccg$getItemStack() {
-		return self().inventory.getStackInSlot(0);
+		var thiz = self();
+		var inventory = thiz.inventory;
+		for (var i = 0; i < inventory.getSlots(); i++) {
+			var stackInSlot = inventory.getStackInSlot(i);
+			if (!stackInSlot.isEmpty()) return stackInSlot;
+		}
+		return null;
 	}
 }

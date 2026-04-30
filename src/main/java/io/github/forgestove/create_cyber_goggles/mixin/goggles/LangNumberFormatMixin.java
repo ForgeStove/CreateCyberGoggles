e4e5b84376939a88afb3dd9b5351bffcf965e1f8
ForgeStove <ForgeStove@users.noreply.github.com>
2026-05-01 00:24:58 +0800

    优化LangNumberFormatMixin，避免在服务端执行格式化逻辑

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/goggles/LangNumberFormatMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/goggles/LangNumberFormatMixin.java
@@ -6,11 +6,14 @@ import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.text.NumberFormat;
+
+import static io.github.forgestove.create_cyber_goggles.core.util.CCGUtil.isServer;
 @Mixin(LangNumberFormat.class)
 public abstract class LangNumberFormatMixin {
 	@Inject(method = "format", at = @At("HEAD"), cancellable = true)
 	private static void format(double d, CallbackInfoReturnable<String> cir) {
 		if (!CCG.config.goggles.preciseNumber) return;
+		if (isServer()) return;
 		if (d == (long) d) return;
 		if (Math.abs(d) < 1E-3) {
 			cir.setReturnValue(String.format("%e", d));

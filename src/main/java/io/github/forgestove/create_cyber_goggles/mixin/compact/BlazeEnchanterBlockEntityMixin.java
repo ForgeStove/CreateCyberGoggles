9cb7dd10274daab20eb42d7b960e3c99bce94367
ForgeStove <ForgeStove@users.noreply.github.com>
2026-07-27 15:53:56 +0800

    修复烈焰人附魔室修改附魔等级后错误地显示旧预测内容的问题

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/compact/BlazeEnchanterBlockEntityMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/compact/BlazeEnchanterBlockEntityMixin.java
@@ -1,21 +1,26 @@
 package io.github.forgestove.create_cyber_goggles.mixin.compact;
-import io.github.forgestove.create_cyber_goggles.core.api.ItemRenderable;
+import io.github.forgestove.create_cyber_goggles.core.api.*;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.*;
 import plus.dragons.createenchantmentindustry.common.processing.enchanter.*;
 @Pseudo
 @Mixin(BlazeEnchanterBlockEntity.class)
-public abstract class BlazeEnchanterBlockEntityMixin implements ItemRenderable {
+public abstract class BlazeEnchanterBlockEntityMixin implements ItemRenderable, Self<BlazeEnchanterBlockEntity> {
 	@Unique public ItemStack ccg$cachedResult = ItemStack.EMPTY;
+	@Unique public int ccg$lastEnchantLevel;
 	@Shadow protected EnchanterBehaviour enchanter;
 	@Shadow protected ItemStack heldItem;
 	@Override
 	public ItemStack ccg$getItemStack() {
-		if (ccg$cachedResult.isEmpty()) ccg$cachedResult = enchanter.getResult(heldItem.copy());
-		if (isActive()) return ccg$cachedResult;
+		if (thiz().isActive()) {
+			var copy = heldItem.copy();
+			if (ccg$cachedResult.isEmpty() || enchanter.value != ccg$lastEnchantLevel) {
+				ccg$cachedResult = enchanter.getResult(copy);
+				ccg$lastEnchantLevel = enchanter.value;
+			}
+			return ccg$cachedResult;
+		}
 		ccg$cachedResult = ItemStack.EMPTY;
-		return heldItem;
+		return heldItem.copy();
 	}
-	@Shadow
-	public abstract boolean isActive();
 }

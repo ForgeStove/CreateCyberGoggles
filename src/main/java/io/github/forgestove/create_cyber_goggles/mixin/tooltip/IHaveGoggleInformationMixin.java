c932b0dc49d56b4f48e367c79db5ff3d48f37c99
ForgeStove <ForgeStove@users.noreply.github.com>
2026-08-08 14:31:43 +0800

    修复被口渴的overwrite的问题

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/tooltip/IHaveGoggleInformationMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/tooltip/IHaveGoggleInformationMixin.java
@@ -1,4 +1,6 @@
 package io.github.forgestove.create_cyber_goggles.mixin.tooltip;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
 import com.simibubi.create.foundation.utility.CreateLang;
 import io.github.forgestove.create_cyber_goggles.CCG;
@@ -7,24 +9,15 @@ import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 @Mixin(IHaveGoggleInformation.class)
 public interface IHaveGoggleInformationMixin {
-	@Inject(method = "containedFluidTooltip", at = @At("HEAD"), cancellable = true)
-	private void containedFluidTooltip(
-		List<Component> tooltip,
-		boolean isPlayerSneaking,
-		IFluidHandler handler,
-		CallbackInfoReturnable<Boolean> cir
+	@WrapMethod(method = "containedFluidTooltip")
+	private boolean containedFluidTooltip(List<Component> tooltip, boolean isPlayerSneaking, IFluidHandler handler, Operation<Boolean> original
 	) {
-		if (!CCG.config.tooltip.fluidContainer) return;
-		if (handler == null || handler.getTanks() == 0) {
-			cir.setReturnValue(false);
-			return;
-		}
+		if (!CCG.config.tooltip.fluidContainer) return original.call(tooltip, isPlayerSneaking, handler);
+		if (handler == null || handler.getTanks() == 0) return original.call(tooltip, isPlayerSneaking, handler);
 		CreateLang.translate("gui.goggles.fluid_container").forGoggles(tooltip);
 		var isEmpty = true;
 		for (var i = 0; i < handler.getTanks(); i++) {
@@ -35,11 +28,10 @@ public interface IHaveGoggleInformationMixin {
 		}
 		if (handler.getTanks() > 1) {
 			if (isEmpty && !tooltip.isEmpty()) tooltip.removeLast();
-			cir.setReturnValue(true);
-			return;
+			return true;
 		}
 		if (isEmpty) for (var i = 0; i < handler.getTanks(); i++)
 			CCGLang.fluidEntry(FluidStack.EMPTY, handler.getTankCapacity(i)).forGoggles(tooltip);
-		cir.setReturnValue(true);
+		return true;
 	}
 }

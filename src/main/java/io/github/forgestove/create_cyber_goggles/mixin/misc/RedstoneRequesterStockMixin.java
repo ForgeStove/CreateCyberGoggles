030c236f498bce2f77f0f233dd44bc34a533939a
ForgeStove <ForgeStove@users.noreply.github.com>
2026-08-31 23:49:27 +0800

    简化

--- src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/RedstoneRequesterStockMixin.java
+++ src/main/java/io/github/forgestove/create_cyber_goggles/mixin/misc/RedstoneRequesterStockMixin.java
@@ -9,19 +9,19 @@ import java.util.*;
 /** 给红石请求器提供客户端网络库存快照（服务端通过 LogisticalStockResponsePacket 回复） */
 @Mixin(RedstoneRequesterBlockEntity.class)
 public abstract class RedstoneRequesterStockMixin implements StockSnapshotHolder {
-	@Unique private InventorySummary ccg$clientsideStockSnapshot;
+	@Unique private InventorySummary ccg$stockSnapshot;
 	@Unique private List<BigItemStack> ccg$newlyReceivedStock;
 	@Override
 	public InventorySummary ccg$getStockSnapshot() {
-		return ccg$clientsideStockSnapshot;
+		return ccg$stockSnapshot;
 	}
 	@Override
 	public void ccg$receiveStockPacket(List<BigItemStack> items, boolean lastPacket) {
 		if (ccg$newlyReceivedStock == null) ccg$newlyReceivedStock = new ArrayList<>();
 		ccg$newlyReceivedStock.addAll(items);
 		if (!lastPacket) return;
-		ccg$clientsideStockSnapshot = new InventorySummary();
-		ccg$newlyReceivedStock.forEach(bigStack -> ccg$clientsideStockSnapshot.add(bigStack));
+		ccg$stockSnapshot = new InventorySummary();
+		ccg$stockSnapshot.addAllBigItemStacks(ccg$newlyReceivedStock);
 		ccg$newlyReceivedStock = null;
 	}
 }
